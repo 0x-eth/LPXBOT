78dc225f33b2ad5f1897d8a431313fe064a4baae
0x <78288949+0x-eth@users.noreply.github.com>
2026-08-13 17:29:02 +0800

    chore: auto-sync 2026-08-13 17:29:02 +0800

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -30,6 +32,14 @@ export default tseslint.config(
       globals: {
         ...globals.browser,
       },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["error", { allowConstantExport: true }],
     },
   },
   prettier,
